4597d18af46b9591b73a203d7bde863ed069514a
vinod.pahuja <vinod.pahuja@1c88fc3c-09b5-11df-a26f-8b716316a5bc>
2013-03-19 10:49:46 +0000

    AD,ARB-RS

--- AD/src/ad/server/module/handler/ADModuleHandler.java
+++ AD/src/ad/server/module/handler/ADModuleHandler.java
@@ -1,20 +1,26 @@
 package ad.server.module.handler;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
 
+import rnd.mywt.client.rpc.ApplicationRequest;
+import rnd.mywt.client.rpc.ApplicationResponse;
+import rnd.mywt.client.rpc.util.ARUtils;
+import rnd.mywt.server.application.AbstractModuleHandler;
+import rnd.mywt.server.util.AppBeanUtils;
 import ad.server.Application;
 import ad.server.ApplicationBean;
 import ad.server.Form;
 import ad.server.Module;
 import ad.server.View;
-import rnd.mywt.client.rpc.ApplicationRequest;
-import rnd.mywt.client.rpc.ApplicationResponse;
-import rnd.mywt.client.rpc.util.ARUtils;
-import rnd.mywt.server.application.AbstractModuleHandler;
 
 public class ADModuleHandler extends AbstractModuleHandler {
 
+	public ADModuleHandler() {
+		super("AD");
+	}
+	
 	@Override
 	public void initModule() {
 		registerApplicationBean("Application", Application.class);
@@ -25,25 +31,22 @@ public class ADModuleHandler extends AbstractModuleHandler {
 	}
 
 	@Override
-	public String getModuleName() {
-		return "AA";
-	}
-
-	@Override
 	public void handleRequest(ApplicationRequest req, ApplicationResponse resp) {
 		super.handleRequest(req, resp);
 
 		switch (req.getMethod()) {
 
 		case Load:
-			Collection children = null;
+			Collection sourceBeans = null;
 			if (ARUtils.getAppBeanName(req).equals("Module")) {
-				children = getObjectPersistor().findAllObject(//
+				sourceBeans = getObjectPersistor().findAllObject(//
 						new Object[] { "ApplicationId = (Select ApplicationId From Applicaion Where Name = ? )" }, //
 						new Object[] { ARUtils.getParent(req) }, //
 						Module.class);
 			}
-			resp.setResult((Serializable) children);
+			ArrayList targetBeans = new ArrayList();
+			AppBeanUtils.copyAllBean(sourceBeans, targetBeans, AppBeanUtils.getServerCopyBeanCtx(), AppBeanUtils.getClientBeanCopyCtx());
+			resp.setResult((Serializable) targetBeans);
 			return;
 		}
 
